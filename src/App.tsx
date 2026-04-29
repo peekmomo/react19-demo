19208a26fd21c7ca5339e5f5859b7442f883190b
peekmomo <1804131510@qq.com>
2026-04-29 23:28:15 +0800

    css in js

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-//在哪儿用在哪儿引入即可
-//react没有全局组件和局部组件的概念
-import Card from "./components/Card"
-import Test from "./components/Test"
-function App() {
+import React, { } from 'react';
+import styled from 'styled-components';
+const ButtonBase = styled.button`
+   padding: 10px 20px;
+   border-radius: 5px;
+   color: white;
+   cursor: pointer;
+   margin: 10px;
+   &:hover {
+     color: red;
+   }
+`;
 
-  const fn = (params:string) => {
-    console.log('子组件触发了 父组件的事件',params)
-  }
-
+const BlueButton = styled(ButtonBase)`
+   background-color: blue;
+   border-radius: 20px;
+`;
+const FailButton = styled(ButtonBase)`
+   background-color: red;
+`;
+const TextButton = styled(ButtonBase)`
+   background-color: transparent;
+   color: blue;
+`;
+const App: React.FC = () => {
   return (
     <>
-      <Card></Card>
-      <button onClick={()=>window.onShow()}>确认</button>
-      <Test callback={fn} title={'测试'}></Test>
+       <BlueButton>普通按钮</BlueButton>
+       <FailButton>失败按钮</FailButton>
+       <TextButton>文字按钮</TextButton>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
